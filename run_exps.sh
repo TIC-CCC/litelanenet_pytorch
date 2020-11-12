529904b2e03fbbf680f150443d0eaa6cdf3aa763
陈浩南 <haonan.chen@mall-ai.com>
2020-11-12 18:12:39 +0800

    update exps

--- run_exps.sh
+++ run_exps.sh
@@ -1,3 +1,6 @@
 python train.py exp_configs/pre_exps/erfnet.yml
 python train.py exp_configs/pre_exps/llnet.yml
 python train.py exp_configs/pre_exps/llnet_prm.yml
+#python train.py exp_configs/pre_exps/llnet_crd_prm.yml
+#python train.py exp_configs/pre_exps/llnet_ds_prm.yml
+#python train.py exp_configs/pre_exps/llnet_ds.yml#
